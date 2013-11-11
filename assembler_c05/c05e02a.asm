cce800f37ba96c2a43dbed3225aff4827b0bfaba
Mariusz Wiśniewski <mariusz.bonifacy@gmail.com>
2013-11-11 13:30:58 +0100

    poprawka

--- assembler_c05/c05e02a.asm
+++ assembler_c05/c05e02a.asm
@@ -1,7 +1,7 @@
-.include "m16def.inc" ;dyrektywa dla preprocesora
-
-.equ liczba1 = 0x22222222 ;32 bit / 4 byte
-.equ liczba2 = 0x11111111 ;32 bit / 4 byte
+.include "m16def.inc"
+ 
+.equ a = 0x11111111 ;liczba 16bitowa
+.equ b = 0x22222222 ;liczba 16bitowa
 
 ldi r24, low(ramend) ;zaladuj do r16 LSB adresu KONCA pamieci SRAM
 out spl, r24 ;sp - wskaznik do stosu, tu LSB, INICJALIZACJA SP
@@ -9,32 +9,32 @@ out spl, r24 ;sp - wskaznik do stosu, tu LSB, INICJALIZACJA SP
 ldi r24, high(ramend) ;zaladuj do r16 MSB adresu KONCA pamieci SRAM
 out sph, r24 ;starsza czesc wskaznika stosu (stack pointer)
 ;stack pointer: 0x045f (czyli MSB == 04)
+ 
+ldi r16, low(a); zaladuj do rejestru
+ldi r17, high(a); analogicznie
+ldi r18, byte3(a)
+ldi r19, byte4(a)
 
-ldi r16, low(liczba1)
-ldi r17, high(liczba1)
-ldi r18, byte3(liczba1)
-ldi r19, byte4(liczba1)
-
-ldi r20, low(liczba2)
-ldi r21, high(liczba2)
-ldi r22, byte3(liczba2)
-ldi r23, byte4(liczba2)
+ldi r20, low(b); zaladuj do rejestru
+ldi r21, high(b); analogicznie
+ldi r22, byte3(b)
+ldi r23, byte4(b)
 
 call plus
 call minus
 
-rjmp pc
+rjmp pc; infinity loop
 
 plus:
-add r20, r16
-add r21, r17
-add r22, r18
-add r23, r19
-ret
+	add r20, r16; 
+	adc r21, r17; 
+	adc r22, r18; 
+	adc r23, r19; 
+	ret
 
 minus:
-sub r20, r16
-sub r21, r17
-sub r22, r18
-sub r23, r19
-ret
+	sub r20, r16; 
+	sbc r21, r17; 
+	sbc r22, r18; 
+	sbc r23, r19; 
+	ret
